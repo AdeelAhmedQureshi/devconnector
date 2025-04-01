71da22ab3e08320c36024f43104c32942a799169
adeel localrepo <adeellocalrepo@.com>
2025-04-01 18:44:04 +0500

    Set up user register post request in user.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const posts = require('./routes/api/posts');
 
 
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 //DB config
 const db = require('./config/keys').mongoURI; 
 
