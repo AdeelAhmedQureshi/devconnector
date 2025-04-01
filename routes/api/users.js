1f49bd3cfa556aa67a1e11a64068323563c5986d
adeel localrepo <adeellocalrepo@.com>
2025-04-01 19:51:38 +0500

    Added simple login functionality in user.js route

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const gravatar = require('gravatar');
 
-//Load user model
+//Load user model(mongooseDB schema)
 const User = require('../models/User')
 // @route GET api/users/test
 // @desc  Test users route
@@ -43,6 +43,31 @@ router.post('/register',(req,res)=> {
             })
         }
      })
-})
+});
+
+// @route GET api/users/login
+// @desc  Login user / Returning JWT Token
+// @access public
+router.post('/login', (req, res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})   // or User.findOne({email: email})
+        .then(user => {
+            if(!user){
+               return res.status(404).json({email: "User not found!"});
+            }
+            // user exit now validating password
+            bcrypt.compare(password, user.password)
+                .then(isMatched => {
+                    //matched
+                    if(isMatched){
+                        res.json({msg: "success"});
+                    }else{
+                        return res.status(400).json({password: "Password Incorrect!"});
+                    }
+                });
+        });
+});
 
 module.exports = router;
